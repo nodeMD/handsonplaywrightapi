5eb09b9b6d350f4bceb475d12dcc4b6c1275de14
nodeMD <mdynieski@pm.me>
2022-08-02 19:16:39 +0200

    [tests] added reusable methods

---
+++ data/incorrectBookingBody.ts
@@ -0,0 +1,11 @@
+export const incorrectBookingBody = {
+  firstname: 23,
+  lastname: 45,
+  totalprice: "no",
+  depositpaid: 92,
+  bookingdates: {
+    checkin: 15,
+    checkout: 1834,
+  },
+  additionalneeds: 32,
+};
